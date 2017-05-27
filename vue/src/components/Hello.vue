3f3b800cd859d8225b2affc18671f07f4a3b85c9
zengcanxin <zengcanxin@dasheng.tv>
2017-05-27 14:43:06 +0800

    --all

---
+++ vue/src/components/Hello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <router-link to="/two">gototwo </router-link>
+    <router-link to="/three">goto three </router-link>
+  </div>
+</template> 
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue/src/components/three.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="three">公用部分2
+	87878797
+    </div>
+</template>
