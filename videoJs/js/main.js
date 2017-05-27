1c47eacb19def343b6ebd83b7bc07e5b9a88526c
zengcanxin <zengcanxin@dasheng.tv>
2017-05-27 10:40:32 +0800

    --all

---
+++ videoJs/js/main.js
@@ -0,0 +1,24 @@
+//live-js
+"use strict";
+
+var daSheng = window.daSheng || {};
+	
+daSheng = {
+
+	bindEvent: function(){
+		
+	},
+
+	checkStatus: function(){
+
+	},
+
+	init: function(){
+		var that = this;
+			that.bindEvent();
+	}
+};
+
+$(function(){
+	daSheng.init();
+});
