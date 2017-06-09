99586c96004ddfaef2712b820ecc856bb97580f3
zengcanxin <zengcanin@dasheng.tv>
2017-06-09 18:02:27 +0800

    --all

--- vue/src/components/three.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="three">公用部分2
-	87878797
-    </div>
-</template>
-
-<script>
-	
-</script>
--- vue/src/components/two.vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<!--测试1-->
-    <div class="two">
-		<router-link to="/">gotoone</router-link>
-    </div>
-</template>
